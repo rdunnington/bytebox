c5d97b92b38bd64be8c1887dc50e32d8c3f8623f
Reuben Dunnington <reuben.dunnington@gmail.com>
2021-11-26 19:56:53 -0800

    generate wasm tests script

---
+++ test/generate_wasm.ps1
@@ -0,0 +1,22 @@
+$wasts = Get-ChildItem "specmirror" -Filter *.wast
+
+if ((Test-Path 'wasm') -eq $false) {
+    mkdir 'wasm'
+}
+
+pushd wasm
+
+foreach ($file in $wasts)
+{
+    $output_dir = [System.IO.Path]::GetFileNameWithoutExtension($file.FullName)
+
+    if ((Test-Path $output_dir) -eq $false) {
+        mkdir $output_dir
+    }
+
+    pushd $output_dir
+    wast2json.exe $file.FullName
+    popd
+}
+
+popd
