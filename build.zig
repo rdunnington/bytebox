a71dd7f330f64d91d0b9fd762711ee3f0d839f70
Reuben Dunnington <reuben.dunnington@gmail.com>
2021-11-28 22:30:57 -0800

    add standalone test runner with ability to filter on suite and individual test name

---
+++ build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("fresh", "test.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const test_step = b.step("test", "Test the app");
+    test_step.dependOn(&run_cmd.step);
+}
