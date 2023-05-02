d9484e85f6223590a303f6b109959627cabf4834
Reuben Dunnington <reuben.dunnington@gmail.com>
2023-05-01 20:10:57 -0700

    fixing UTIME_* values

--- src/wasi.zig
+++ src/wasi.zig
@@ -1012,8 +1012,8 @@ const Helpers = struct {
     }
 
     fn fdFilestatSetTimesPosix(fd: std.os.fd_t, timestamp_wasi_access: u64, timestamp_wasi_modified: u64, fstflags: u32, errno: *Errno) void {
-        const UTIME_NOW: i64 = -2;
-        const UTIME_OMIT: i64 = -1;
+        const UTIME_NOW: i64 = (1 << 30) - 1;
+        const UTIME_OMIT: i64 = (1 << 30) - 2;
 
         var times = [2]std.os.timespec{
             .{ // access time
