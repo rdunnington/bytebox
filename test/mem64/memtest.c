25bc2f60021a5111a57f14f37102282161c845cf
Reuben Dunnington <reuben.dunnington@gmail.com>
2024-05-13 15:44:23 -0700

    updated memtest.c

--- test/mem64/memtest.c
+++ test/mem64/memtest.c
@@ -18,22 +18,26 @@ __attribute__((visibility("default"))) int64_t memtest(int32_t val_i32, int64_t 
 	assert(start_page != -1);
 
 	char* mem = (char*)(start_page);
-	char* mem_stores = mem + MB * 1;
-	char* mem_loads = mem + MB * 2;
+	volatile char* mem_stores = mem + MB * 1;
+	volatile char* mem_loads = mem + MB * 2;
 
 	int64_t num_pages = __builtin_wasm_memory_size(0); // memory.size
 	assert(num_pages >= (PAGES_PER_GB * 2));
+
+	*(int32_t*)(mem_loads + 0) = val_i32; // i32.store
+	*(int64_t*)(mem_loads + 8) = val_i64; // i64.store
+	*(float*)(mem_loads + 16) = val_f32; // f32.store
+	*(double*)(mem_loads + 24) = val_f64; // f64.store
 
 	*(int32_t*)(mem_stores + 0) = *(int32_t*)(mem_loads + 0); // i32.load -> i32.store
 	*(int64_t*)(mem_stores + 8) = *(int64_t*)(mem_loads + 8); // i64.load -> i64.store
 	*(float*)(mem_stores + 16)  = *(float*)(mem_loads + 16); // f32.load -> f32.store
 	*(double*)(mem_stores + 24) = *(double*)(mem_loads + 24); // f64.load -> f64.store
 
-	*(int8_t*)(mem_stores + 32) = (int8_t)val_i32; // i32.store8
-	*(int16_t*)(mem_stores + 40) = (int16_t)val_i32; // i32.store16
-	*(int8_t*)(mem_stores + 48) = (int8_t)val_i64; // i64.store8
-	*(int16_t*)(mem_stores + 56) = (int16_t)val_i64; // i64.store16
-	*(int32_t*)(mem_stores + 64) = (int32_t)val_i64; // i64.store32
+	assert(*(int32_t*)(mem_stores + 0) == val_i32);
+	assert(*(int64_t*)(mem_stores + 8) == val_i64);
+	assert(*(float*)(mem_stores + 16) == val_f32);
+	assert(*(double*)(mem_stores + 24) == val_f64);
 
 	int32_t store32 = 0;
 	store32 += (int32_t)*(int8_t*)(mem_loads + 32); // i32.load8_s
